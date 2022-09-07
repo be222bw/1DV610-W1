85e722b9b33bd6653e4bee21b8f72210af095503
Andreas Lillje <al225wg@student.lnu.se>
2022-09-07 10:18:03 +0200

    Add App class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 public class App {
     public static void main(String[] args) {
-
+        // Consider another variable name
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter file name: ");
+        String fileName = scanner.nextLine();
+        scanner.close();
+        System.out.println(fileName);
+        File file = new File(fileName);
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file, "utf-8");
+        } catch (Exception e) {
+            System.err.println(e.toString());
+          }
+          int numberOfLines = 0;
+        while (fileReader.hasNextLine()) {
+            fileReader.nextLine();
+            numberOfLines++;
+        }
+        System.out.println("Number of lines in document " + fileName + " is " + numberOfLines);
+        fileReader.close();
     }
 }
